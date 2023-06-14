7d9312db234eed6ee1de5ec54325bda1cc8b2a58
jaeyunn <wodbs135@gmail.com>
2023-06-14 13:20:14 +0900

    mock 데이터 및 테스트 레포지토리 관리를 위한 테스트 모듈 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(project(":core:designsystem"))
     implementation(project(":core:data"))
     implementation(project(":core:domain"))
+    implementation(project(":core:testing"))
 
     implementation(AndroidX.CORE_KTX)
     implementation(AndroidX.APP_COMPAT)
